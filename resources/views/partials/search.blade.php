229409bf5526768c212ce7c34776462a95629457
Tang Tang <jim.tang@evolutivelabs.com>
2022-05-25 00:18:25 +0800

    :art:  dd

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -3,5 +3,8 @@
 <form action="/search" method="post">
     <input type="text" name="keyword">
     <button type="submit"><i class="bi bi-search"></i></button>
+    <h1>123</h1>
+    <div>try</div>
+    <table>3</table>
 </form>
 </div>
