d76c34fe426c0e4cfd6eb74558145b5236e643bb
Tang Tang <jim.tang@evolutivelabs.com>
2022-05-25 01:37:29 +0800

    :bug:  delete garbage

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -3,18 +3,6 @@
 <form action="/search" method="post">
     <input type="text" name="keyword">
     <button type="submit"><i class="bi bi-search"></i></button>
-    <h1>123</h1>
-    <div>try</div>
-    <table>3</table>
-    <div>ddddd</div>
-    <h1>dddddddd</h1>
-    <a href="322"></a>
-    <a href="ddddd"></a>
-    <div>ddddddete-dad</div>
-
-    <hr>
-    <br>
-    <p>dddd</p>
 
     <h2>tittle</h2>
     <hr>
