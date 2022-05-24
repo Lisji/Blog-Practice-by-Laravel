da68ec9e2fde39a4eba46fdd42e7629c2982e66e
Tang Tang <jim.tang@evolutivelabs.com>
2022-05-25 02:24:56 +0800

    :sparkles: (add tittle)

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -4,7 +4,17 @@
     <input type="text" name="keyword">
     <button type="submit"><i class="bi bi-search"></i></button>
 
-    <h2>tittle</h2>
+    <h1>title</h1>
+    <hr><hr>
+    <hr><hr>
+    <p>content</p>
+
+    <h2>subtitle</h2>
+    <hr>
+    <hr>
+    <p>content</p>
+
+    <h2>title</h2>
     <hr>
     <hr>
     <p>content</p>
